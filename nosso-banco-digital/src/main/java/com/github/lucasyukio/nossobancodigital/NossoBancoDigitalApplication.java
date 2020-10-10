6cf8b97b3306787a8f638eddfc7544265806234d
Lucas Yukio <lucas.yukioh@hotmail.com>
2020-10-10 18:24:51 -0300

    Add Swagger.

--- nosso-banco-digital/src/main/java/com/github/lucasyukio/nossobancodigital/NossoBancoDigitalApplication.java
+++ nosso-banco-digital/src/main/java/com/github/lucasyukio/nossobancodigital/NossoBancoDigitalApplication.java
@@ -7,7 +7,15 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @SpringBootApplication
+@EnableSwagger2
 @EnableAsync
 public class NossoBancoDigitalApplication {
 
@@ -18,6 +26,15 @@ public class NossoBancoDigitalApplication {
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 	
 }
